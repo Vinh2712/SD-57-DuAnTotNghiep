a655431f9e9484cd987c68307783c390b58ba3ba
Vinh2712 <vinhnqph28348@fpt.edu.vn>
2024-06-26 22:39:29 +0700

    code 26-6

---
+++ src/main/java/com/example/sd_57_datn/Sd57DatnApplication.java
@@ -0,0 +1,11 @@
+package com.example.sd_57_datn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sd57DatnApplication {
+
+    public static void main(String[] args) { SpringApplication.run(Sd57DatnApplication.class, args); }
+
+}
---
+++ src/test/java/com/example/sd_57_datn/Sd57DatnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sd_57_datn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sd57DatnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
