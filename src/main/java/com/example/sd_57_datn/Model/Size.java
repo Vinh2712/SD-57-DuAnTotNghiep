7c19fc87b089173ba6ad57d0e7d7c05c77571590
Quyenndph26345 <117371989+Quyenndph26345@users.noreply.github.com>
2024-06-26 23:09:42 +0700

    26-6

---
+++ src/main/java/com/example/sd_57_datn/Model/Size.java
@@ -0,0 +1,45 @@
+package com.example.sd_57_datn.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "Size")
+public class Size {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "Id_Size")
+    private UUID id;
+
+    @Column(name = "size")
+    private String size;
+
+    @Column(name = "ghiChu")
+    private String ghiChu;
+
+    @Column(name = "ngayTao")
+    private String ngayTao;
+
+    @Column(name = "ngaySua")
+    private String ngaySua;
+
+    @Column(name = "trangThai")
+    private int trangThai;
+
+}
